ce153557fa56c06caa2c19128db82283c3f17b32
Kanstancin <kastus.novikau@gmail.com>
2023-04-22 20:47:09 +0300

    Add option to fetch a rate

--- database.py
+++ database.py
@@ -1,5 +1,24 @@
-import mysql.connector
+from datetime import datetime, date
 import nbrb
+import mysql.connector
+
+
+class QueryResult:
+    def __init__(self, cursor):
+        self._cursor = cursor
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._cursor:
+            self._cursor.close()
+
+    def fetch_one(self):
+        return self._cursor.fetchone()
+
+    def fetch_all(self):
+        return self._cursor.fetchall()
 
 
 class StorageDatabase:
@@ -25,39 +44,26 @@ class StorageDatabase:
         if self._connection:
             self._connection.close()
 
-    @staticmethod
-    def mysql_transform(obj) -> dict | list:
-        from dataclasses import fields, is_dataclass
-        from datetime import datetime, date
+    def _raise_on_no_connection(self):
+        if self._connection is None:
+            raise ConnectionError(f'Connection to {self.user}@{self.host}:{self.port}/{self.database} is closed.')
 
-        def mysql_type_prepare(value):
-            if isinstance(value, datetime):
-                return value.strftime('%Y-%m-%d %H:%M:%S')
-            if isinstance(value, date):
-                return value.strftime('%Y-%m-%d')
-            return value
-
-        def transform(value):
-            if not is_dataclass(value):
-                raise ValueError(f'The object {value.__class__.__name__} is not a dataclass')
-
-            return {
-                field.name: mysql_type_prepare(getattr(value, field.name))
-                for field in fields(value)
-            }
-
-        if isinstance(obj, list):
-            return [transform(val) for val in obj]
-        return transform(obj)
-
-    def _execute_write_query(self, query: str, parameters: dict | list[dict] | None = None):
+    def _execute_query(self, query: str, parameters: dict | list[dict] | None = None):
         cursor = self._connection.cursor(prepared=True)
         if isinstance(parameters, dict):
             cursor.execute(query, parameters)
         else:
             cursor.executemany(query, parameters)
+        return cursor
+
+    def _execute_write_query(self, query: str, parameters: dict | list[dict] | None = None):
+        cursor = self._execute_query(query, parameters)
         self._connection.commit()
         cursor.close()
+
+    def _execute_read_query(self, query: str, parameters: dict | list[dict] | None = None):
+        cursor = self._execute_query(query, parameters)
+        return QueryResult(cursor)
 
     def connect(self):
         self._connection = mysql.connector.connect(
@@ -94,7 +100,7 @@ class StorageDatabase:
             "    %(date_end)s"
             ")"
         )
-        currency = StorageDatabase.mysql_transform(currency)
+        currency = mysql_transform(currency)
         self._execute_write_query(query, currency)
 
     def update_rates(self, rate: nbrb.Rate | list[nbrb.Rate]):
@@ -111,5 +117,47 @@ class StorageDatabase:
             "    %(rate)s"
             ")"
         )
-        rate = StorageDatabase.mysql_transform(rate)
+        rate = mysql_transform(rate)
         self._execute_write_query(query, rate)
+
+    def get_rate(self, currency: nbrb.Currency, on_date: date = date.today()) -> nbrb.Rate | None:
+        self._raise_on_no_connection()
+        query = "SELECT * FROM rate WHERE date = %(date)s and currency_id = %(currency_id)s"
+        with self._execute_read_query(query, {
+            "date": mysql_type_prepare(on_date),
+            "currency_id": currency.internal_id
+        }) as result:
+            row = result.fetch_one()
+            return nbrb.Rate(*row) if row else None
+
+    def get_currency_by_abbreviation(self, abbreviation: str) -> nbrb.Currency | None:
+        self._raise_on_no_connection()
+        query = "SELECT * FROM currency WHERE abbreviation = %(abbreviation)s"
+        with self._execute_read_query(query, {"abbreviation": abbreviation}) as result:
+            row = result.fetch_one()
+            return nbrb.Currency(*row) if row else None
+
+
+def mysql_type_prepare(value):
+    if isinstance(value, datetime):
+        return value.strftime('%Y-%m-%d %H:%M:%S')
+    if isinstance(value, date):
+        return value.strftime('%Y-%m-%d')
+    return value
+
+
+def mysql_transform(obj) -> dict | list:
+    from dataclasses import fields, is_dataclass
+
+    def transform(value):
+        if not is_dataclass(value):
+            raise ValueError(f'The object {value.__class__.__name__} is not a dataclass')
+
+        return {
+            field.name: mysql_type_prepare(getattr(value, field.name))
+            for field in fields(value)
+        }
+
+    if isinstance(obj, list):
+        return [transform(val) for val in obj]
+    return transform(obj)
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import argparse
-import nbrb
 import database
+import datetime
+import nbrb
 import os
+
+
+def get_database_connection():
+    return database.StorageDatabase(
+            host=os.environ.get('CURR_DB_HOST'),
+            port=int(os.environ.get('CURR_DB_PORT')),
+            user=os.environ.get('CURR_DB_USER'),
+            password=os.environ.get('CURR_DB_PASSWD'),
+            database=os.environ.get('CURR_DB_NAME')
+    )
 
 
 def pure_update():
@@ -9,13 +20,7 @@ def pure_update():
 
     print('Making pure update of databases')
     api_client = nbrb.Client()
-    with database.StorageDatabase(
-            host=os.environ.get('CURR_DB_HOST'),
-            port=int(os.environ.get('CURR_DB_PORT')),
-            user=os.environ.get('CURR_DB_USER'),
-            password=os.environ.get('CURR_DB_PASSWD'),
-            database=os.environ.get('CURR_DB_NAME')
-    ) as db_client:
+    with get_database_connection() as db_client:
         time_start = time.time()
 
         def measure_time(prompt: str):
@@ -35,8 +40,41 @@ def pure_update():
         rates = api_client.all_rates()
         measure_time('Updated local rates')
 
+        # Log dates (and times)
+        dates = [rate.date for rate in rates]
+        min_date, max_date = min(dates), max(dates)
+        print(f'Dates of rates from {min_date} to {max_date}')
+
         db_client.update_rates(rates)
         measure_time('Updated db rates')
+
+
+def print_rate(usd_abbreviation: str, on_date: datetime.date | None = None):
+    on_date = on_date if on_date else datetime.date.today()
+    api_client = nbrb.Client()
+
+    def get_currency(abbreviation: str):
+        if cur := db_client.get_currency_by_abbreviation(abbreviation):
+            return cur
+        return api_client.available_currencies().get(abbreviation, None)
+
+    def get_rate(currency: nbrb.Currency, on_date: datetime.date):
+        if rt := db_client.get_rate(currency, on_date):
+            return rt
+        return api_client.rate(currency, on_date)
+
+    with get_database_connection() as db_client:
+        db_client.connect()
+        # Try to get currency from database
+        if (currency := get_currency(usd_abbreviation)) is None:
+            print(f'Cannot find such a currency {usd_abbreviation}')
+            return
+
+        if (rate := get_rate(currency, on_date)) is None:
+            print(f'Cannot fetch rate for {usd_abbreviation} on date {on_date.strftime("%Y-%m-%d")}')
+            return
+
+        print(f'{usd_abbreviation} ({currency.name_blr}) on {on_date.strftime("%Y-%m-%d")} was {rate.rate} BYN')
 
 
 def main():
@@ -46,11 +84,24 @@ def main():
         action='store_true',
         help='if presented, only database update is performed'
     )
+    parser.add_argument(
+        '--currency-rate',
+        type=str,
+        help='if presented, the rate for currency code is printed'
+    )
+    parser.add_argument(
+        '--date',
+        type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+        help='date for the currency. if no date, today assumed'
+    )
 
     args = parser.parse_args()
 
     if args.pure_update:
         pure_update()
+
+    if args.currency_rate:
+        print_rate(args.currency_rate, args.date)
 
 
 if __name__ == '__main__':
